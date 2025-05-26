d9d8af1b37e7698ca2d7d8df643afef8f944ee82
Reihan Agam <reihanagam7@gmail.com>
2025-05-26 17:50:21 +0700

    update

--- app/components/home/Home.tsx
+++ app/components/home/Home.tsx
@@ -9,14 +9,8 @@ const Home: React.FC = () => {
       <div className="flex flex-col md:flex-row gap-4 w-full max-w-6xl">
 
         <div className="flex-1 relative text-center block md:hidden">
-          <img src="/images/jambore-logo.png" alt="Jambore Nasional 2025" className="mx-auto mb-4" />
-          <div className="flex justify-center gap-4">
-            <img src="/images/motorbike.png" alt="Motorbike Prize" className="absolute left-[45px] bottom-[10px] w-36" />
-            <img src="/images/car.png" alt="Car Prize" className="absolute right-[45px] bottom-[10px] w-36" />
-          </div>
-          <button className="absolute left-[35px] right-[35px] bottom-[-10px] bg-red-600 text-white px-6 py-2 rounded font-bold">
-            OUR DOOR PRIZE
-          </button>
+          <img src="/images/jambore-logo.png" alt="Jambore Nasional 2025" className="mx-auto" />
+          <img src="/images/mobile-our-door-prize.png" alt="Jambore Nasional 2025" className="mx-auto absolute bottom-[-80px]" />
         </div>
 
         <div className="relative flex-1 text-center md:block hidden">
@@ -34,14 +28,14 @@ const Home: React.FC = () => {
           </button>
         </div>
 
-        <div className="flex-1 text-center md:pt-0 pt-[30px]">
+        <div className="flex-1 text-center md:pt-0 pt-[80px]">
           <h2 className="text-2xl font-bold text-black mb-4">OUR SPONSORSHIP</h2>
 
           <div className="justify-items-center">
             <img src="/images/our-sponsorship.png" alt="Car Prize" className=""/>
           </div>
           
-          <div className="mt-10 text-center">
+          <div className="my-10 text-center">
             <p className="font-stencil text-lg mb-2">CLICK BUTTON BELOW FOR MERCH</p>
             <Link href="/order">
               <button className="bg-green-600 text-white text-xl font-bold px-6 py-3 rounded shadow hover:bg-green-700 transition">
